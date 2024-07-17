deffc4edf3389f53f36fadb8a860c751f9439d37
SRIJAN K <srijanbangera@gmail.com>
2024-07-17 10:41:46 +0000

    conversiondatatype, done

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,22 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// Even score is converted to number, it is not a number(NAN)
+
+// "33" => string
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let number = 33
+
+let stringnumber = String(number)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+console.log(stringnumber =! stringnumber);
