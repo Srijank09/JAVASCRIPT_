9b3b678e3d033212be718748d4fcb9868b66ccec
SRIJAN K <srijanbangera@gmail.com>
2024-07-17 08:07:44 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js codes as newer version
+
+// alert(3 + 3) use node js not web browser 
+
+// code readability shoub be high 
+
+// primitive data types
+
+// number=> 2 to the power 53 
+// bigint bigger numbers 
+// string 
+// boolean => true or false
+//  null = standalone value
+//  undefined
+//  symbol= uniqunesss
+
+//  Important null is a object (NOTE)
+// undefinied is a undefined itself
+
+// objects
