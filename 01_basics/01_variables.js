36d9da01700face1a155496846b58a67576da573
SRIJAN K <srijanbangera@gmail.com>
2024-07-17 10:43:18 +0000

    commits

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 12345
+let accountEmail = "Srijan@google.com"
+var accountPassword = "09876"
+accountCity = "Mangalore"
+
+console.log(accountId);
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JAVASCRIPT_
 A simple code snippets
+
+Learnings of Javascript!!!
