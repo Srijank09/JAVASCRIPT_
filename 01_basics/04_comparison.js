834cafa8be81a5ad7fbd3d80af18054c0a1ec740
SRIJAN K <srijanbangera@gmail.com>
2024-07-23 06:34:01 +0000

    comarison operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+console.log(2 > 1);
+
+let x = 3;
+const y = x++;
+
+console.log(x, y);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(a, b);
+// Expected output: "a:4, b:4"
+
+// === used for strict checking
+console.log("2" === 2);
