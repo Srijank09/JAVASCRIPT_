319d9054b06776f25853e1015c226725745521bb
SRIJAN K <srijanbangera@gmail.com>
2024-11-02 09:03:07 +0000

    data types

--- 01_basics/datatypessummary.js
+++ 01_basics/datatypessummary.js
@@ -3,13 +3,33 @@
 // 7 types
 // string, number,  boolean, null(empty), undefined, symbol=unique, BigInt
 
+const value = Symbol('123')
+const anotherValue = Symbol('123')
+
+console.log(value==anotherValue);  //false
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
+
 // reference types (Non primitive)
 
 // Array, Objects, Functions
 
 // ***************************************************//
 
-// stack memory (primitve types)--copy,,, Heap memory (non primitve)--reference
+// stack memory (primitve types)--copy,,>>>> Heap memory (non primitve)--reference
 
 // Values changed in primitive i.e copy id, only copy is changed but,
 
