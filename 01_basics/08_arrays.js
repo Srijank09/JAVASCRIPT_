3ef9ac466ec59612849890e3dbbf85e1b34b2268
SRIJAN K <srijanbangera@gmail.com>
2024-11-02 10:49:32 +0000

    arrays done

--- 01_basics/08_arrays.js
+++ 01_basics/08_arrays.js
@@ -11,18 +11,43 @@ const myArr = new Array(1,2,3,4,5)
 // console.log(myArr.includes(9));
 
 // console.log(myArr);
-console.log("A - ",myArr); //Before
+// console.log("A - ",myArr); //Before
 
-const myn1 = myArr.slice(1,3)
+// const myn1 = myArr.slice(1,3)
 
-console.log(myn1," Slicing");
-console.log("B - ",myArr); //After slicing
+// console.log(myn1," Slicing");
+// console.log("B - ",myArr); //After slicing
 
-const myn2 = myArr.splice(1,3)  
-console.log(myn2," Splicing");
-console.log("C - ",myArr);  //After splicing
+// const myn2 = myArr.splice(1,3)  
+// console.log(myn2," Splicing");
+// console.log("C - ",myArr);  //After splicing
 
 //Splicing manipulates the oroginal array while slicing just slices n-1 index arrays and shows the result
+
+// *************************************************
+
+const cars = ["porsche", "alto", "Nano"]
+const bikes = ["duke","apache","Xpulse"]
+
+// cars.push(bikes)
+// console.log(cars);
+
+// const vehciles = cars.concat(bikes)
+// console.log(vehciles);
+
+// Spreading**
+
+// const allnewvehicles = [...cars, ...bikes]
+// console.log(allnewvehicles);
+
+// Array.flat(infinity) ---spreds and gives normal array from all combined array inside arrays
+
+// console.log(Array.isArray('Srijan'));
+// console.log(Array.from('Srijan')); //---creates an array using from keyword
+
+// console.log(Array.of(set of elements));
+
+
 
 
 
